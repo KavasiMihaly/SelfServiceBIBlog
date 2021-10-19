afcfea1c3e0a1e22eaed80bd3257d5b2bf8ed368
KavasiMihaly <kavasi.mihaly@outlook.com>
2021-10-19 14:06:00 +0100

    update
    new to Documentation pbi report and automate tabular editor ps script both WIP

---
+++ TabularEditor/Automation/AutomateTabularEditor.ps1
@@ -0,0 +1,4 @@
+﻿Write-Output("Start of Code")
+$p = Start-Process -filePath "C:\Program Files (x86)\Tabular Editor\TabularEditor.exe" -Wait -NoNewWindow -PassThru `
+       -ArgumentList "`"Provider=MSOLAP;Data Source=powerbi://api.powerbi.com/v1.0/myorg/workspace;User ID=userid;Password=password;Persist Security Info=True;Impersonation Level=Impersonate`" `"dataset`" -S `"script-path`" -D `"powerbi://api.powerbi.com/v1.0/myorg/workspace`" `"dataset`" -O"
+Write-Output("End of Code")
